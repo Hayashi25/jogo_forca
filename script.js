34d212dd3a52968b84fabc4cff6128f65e793579
Gabriela <gabrielahpw@gmail.com>
2019-07-21 00:29:01 -0300

    a letra

--- script.js
+++ script.js
@@ -14,37 +14,37 @@ let ii = document.querySelector("#ii");
 function apertar(){
     
     if (label.value == frutas[0]) {
-        alert("vc acertou a palavra");
+        alert("vc acertou a letra");
         m.innerHTML += frutas[0];
     }
     
     else if(label.value == frutas[1]){
-        alert("vc acertou a palavra");
+        alert("vc acertou a letra");
         u.innerHTML += frutas[1]();   
     }
     
     else if(label.value == frutas[2]){   
-        alert("vc acertou a palavra");
+        alert("vc acertou a letra");
         r.innerHTML += frutas[2];  
     }
     
     else if(label.value == frutas[3]){
-        alert("vc acertou a palavra");
+        alert("vc acertou a letra");
         i.innerHTML += frutas[3];  
     }
     
     else if(label.value == frutas[4]){ 
-        alert("vc acertou a palavra");
+        alert("vc acertou a letra");
         c.innerHTML += frutas[4];  
     }
     
     else if(label.value == frutas[5]){
-        alert("vc acertou a palavra ");  
+        alert("vc acertou a letra ");  
         ii.innerHTML += frutas[5];   
     }
 
     else{
-        alert("vc errou a palavra");    
+        alert("vc errou a letra");    
     }
 }
 
